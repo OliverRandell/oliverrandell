86cb4827e276314373c268b09f4596e2b707c86c
Oliver Randell <oliverrandell@Olivers-MacBook-Pro.local>
2023-07-03 17:56:00 +1000

    Fixing problems

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Me from './Me';
 import Projects from './Projects';
 import Work from './Work';
@@ -7,14 +7,14 @@ import Education from './Education';
 
 const Pages = ({ user }) => {
   return (
-    <Router basename="/">
+    <HashRouter basename="/">
       <Routes>
         <Route exact path="/" element={<Me user={user} />} />
         <Route path="/projects" element={<Projects user={user} />} />
         <Route path="/work" element={<Work user={user} />} />
         <Route path="/education" element={<Education user={user} />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
